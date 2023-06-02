2ff8eb04fb72befa352e51ea8eb49e03506e6cb4
unknown <facusosa1607@gmail.com>
2023-06-02 19:27:29 -0300

    Agregando Viewmodel

---
+++ app/src/main/java/ar/edu/unlam/mobile2/presentation/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package ar.edu.unlam.mobile2.presentation
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+}
--- app/src/main/java/ar/edu/unlam/mobile2/ui/ui/theme/MainViewModel.kt
+++
@@ -1,6 +0,0 @@
-package ar.edu.unlam.mobile2.ui.ui.theme
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel:ViewModel() {
-}
