24da12aae6ac7a35bf79c5cfa1fa4960c1021e2e
CatramboneMauro <maurojose029@gmail.com>
2023-06-12 16:39:52 -0300

    Cambios

--- app/src/main/java/ar/edu/unlam/mobile2/core/data/local/MovieDao.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/core/data/local/MovieDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import ar.edu.unlam.mobile2.core.data.local.entity.MovieEntity
+import ar.edu.unlam.mobile2.core.data.local.entity.MovieFavouriteEntity
 
 @Dao
 interface MovieDao {
@@ -17,4 +18,19 @@ interface MovieDao {
         """
     )
     suspend fun getMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavouriteMovie(movie: MovieFavouriteEntity)
+
+    @Query("""
+        DELETE FROM MovieFavouriteEntity WHERE idUser = :idUser AND id = :movieId
+        """)
+    suspend fun deleteFavouriteMovie(movieId: Int, idUser: Int)
+
+    @Query(
+        """
+        SELECT * FROM MovieFavouriteEntity MOVIE_FAVOURITE JOIN UserEntity USER ON MOVIE_FAVOURITE.idUser = USER.idUser WHERE USER.idUser = :idUser ORDER BY MOVIE_FAVOURITE.added_date DESC
+        """
+    )
+    suspend fun getLikedMovies(idUser: Int): List<MovieFavouriteEntity>
 }
--- app/src/main/java/ar/edu/unlam/mobile2/core/data/local/MovieDatabase.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/core/data/local/MovieDatabase.kt
@@ -4,8 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import ar.edu.unlam.mobile2.core.data.local.MovieDao
 import ar.edu.unlam.mobile2.core.data.local.entity.MovieEntity
+import ar.edu.unlam.mobile2.core.data.local.entity.MovieFavouriteEntity
+import ar.edu.unlam.mobile2.core.data.local.entity.UserEntity
 
-@Database(entities = [MovieEntity::class], version = 1)
+@Database(entities = [MovieEntity::class,MovieFavouriteEntity::class,UserEntity::class], version = 1)
 abstract class MovieDatabase : RoomDatabase() {
     abstract val dao: MovieDao
 }
