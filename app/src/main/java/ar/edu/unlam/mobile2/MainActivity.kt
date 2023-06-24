6a4a6478fb1728e769e49161f35d129f9d7ac571
nahue <nahuelpachano18@gmail.com>
2023-06-24 17:05:56 -0300

    "Agregando splash screen"

--- app/src/main/java/ar/edu/unlam/mobile2/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ar.edu.unlam.mobile2.ui.MyMoviesApp
 import ar.edu.unlam.mobile2.ui.theme.MyMoviesTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MyMoviesTheme {
                 Surface(
