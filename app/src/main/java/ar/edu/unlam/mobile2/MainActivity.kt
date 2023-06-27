109f059fcc6e75e728559954fc19be1ef7e4903c
FacuSosa <facusosa1607@gmail.com>
2023-06-26 21:05:51 -0300

    "cambios"

--- app/src/main/java/ar/edu/unlam/mobile2/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/MainActivity.kt
@@ -8,6 +8,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import ar.edu.unlam.mobile2.ui.MyMoviesApp
 import ar.edu.unlam.mobile2.ui.theme.MyMoviesTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,33 +28,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                     MyMoviesApp()
 
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "HOME") {
-                        composable("HOME") {
-                            HomeScreen(onMovieClick = {
-                                navController.navigate("DETAIL/${it.id}")
-                            })
-                        }
-                        composable(
-                            "DETAIL/{movie_id}",
-                            arguments = listOf(
-                                navArgument("movie_id") {
-                                    type = NavType.IntType
-                                }
-                            )
-                        ) {
-                            DetailScreen(onBack = {
-                                navController.popBackStack()
-                            }
-                        )
                         }
                     }
 
                 }
             }
         }
-    }
-}
+
